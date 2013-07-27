890be0b79bab4af6dde45c460049202f740f58f9
clapyourhouse <08m078@gmail.com>
2013-07-27 15:18:58 +0900

    Initial Commit

---
+++ sicotawaAppCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sicotawaAppCamera
+//
+//  Created by KitamuraShogo on 13/07/27.
+//  Copyright (c) 2013年 KitamuraShogo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sicotawaAppCamera/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sicotawaAppCamera
+//
+//  Created by KitamuraShogo on 13/07/27.
+//  Copyright (c) 2013年 KitamuraShogo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
